c3626abe52b337094a3af01653c0bb88fe6f6e07
Ibadetegashi <77028232+Ibadetegashi@users.noreply.github.com>
2022-03-08 23:11:13 +0100

    init+navbar

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import 'bootstrap';
+import 'jquery'
+import 'popper.js'
+import './assets/app.scss'
+
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <NavBar></NavBar>
+     
+     
+
+      <div class="container h-100">
+        <div class="row p-5 h-100 justify-content-center align-items-center">
+          <div class="col-md-6">
+            <img src="/img/svg/product.svg" alt="" width="400">
+          </div>
+         
+        </div>
+      </div>
+      
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    NavBar
+  }
+};
+</script>
+
+<style>
+
+</style>
+
