5b549d34f8d16be8d94c69aa1dedfcf5b6ebe7cc
Ibadetegashi <77028232+Ibadetegashi@users.noreply.github.com>
2022-03-09 22:14:29 +0100

    FirebaseConfig, login&singup methods

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -31,9 +31,9 @@
                                 <input type="password" @keyup.enter="login" v-model="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                             </div>
 
-                             <div class="form-group">
+                              <div class="form-group">
                                 <button class="btn btn-primary" @click="login">Login</button>
-                            </div>
+                            </div> 
 
                         </div>
                         <div class="tab-pane fade" id="pills-register" role="tabpanel" aria-labelledby="pills-register-tab">
@@ -71,77 +71,80 @@
   </div>
 </template>
 
-// <script>
+<script>
 
-// import {fb,db} from '../firebase'
+import {fb} from '../firebase'
+ //import $ from 'jquery'
 
-// export default {
-//   name: "Login",
-//   props: {
-//     msg: String
-//   },
-//   data(){
-//       return {
-//           name:null,
-//           email:null,
-//           password:null
-//       }
-//   },
 
-//   methods:{
-//       login(){
+export default {
+  name: "Login",
+  props: {
+    msg: String
+  },
+  data(){
+      return {
+          name:null,
+          email:null,
+          password:null
+      }
+  },
 
-//           fb.auth().signInWithEmailAndPassword(this.email, this.password)
-//                         .then(() => {
-//                         $('#login').modal('hide')
-//                           this.$router.replace('admin');  
-//                         })
-//                         .catch(function(error) {
-//                             // Handle Errors here.
-//                             var errorCode = error.code;
-//                             var errorMessage = error.message;
-//                             if (errorCode === 'auth/wrong-password') {
-//                                 alert('Wrong password.');
-//                             } else {
-//                                 alert(errorMessage);
-//                             }
-//                             console.log(error);
-//                     });
+  methods:{
+       login(){
 
-//       },
-//       register(){
-//             fb.auth().createUserWithEmailAndPassword(this.email, this.password)
-//                 .then((user) => {
-//                     $('#login').modal('hide')
+          fb.auth().signInWithEmailAndPassword(this.email, this.password)
+                        // .then(() => {
+                        // $('#login').modal('hide')
+                        //   this.$router.replace('admin');  
+                        // })
+                        .catch(function(error) {
+                            // Handle Errors here.
+                            var errorCode = error.code;
+                            var errorMessage = error.message;
+                            if (errorCode === 'auth/wrong-password') {
+                                alert('Wrong password.');
+                            } else {
+                                alert(errorMessage);
+                            }
+                            console.log(error);
+                    });
+
+      },
+    
+      register(){
+            fb.auth().createUserWithEmailAndPassword(this.email, this.password)
+                // .then((user) => {
+                //     $('#login').modal('hide')
                     
-//                     db.collection("profiles").doc(user.user.uid).set({
-//                         name: this.name
-//                     })
-//                     .then(function() {
-//                         console.log("Document successfully written!");
-//                     })
-//                     .catch(function(error) {
-//                         console.error("Error writing document: ", error);
-//                     });
+                //     db.collection("profiles").doc(user.user.uid).set({
+                //         name: this.name
+                //     })
+                //     .then(function() {
+                //         console.log("Document successfully written!");
+                //     })
+                //     .catch(function(error) {
+                //         console.error("Error writing document: ", error);
+                //     });
 
-//                     this.$router.replace('admin');
-//                 })
-//                 .catch(function(error) {
-//                 // Handle Errors here.
-//                 var errorCode = error.code;
-//                 var errorMessage = error.message;
-//                 if (errorCode == 'auth/weak-password') {
-//                     alert('The password is too weak.');
-//                 } else {
-//                     alert(errorMessage);
-//                 }
-//                 console.log(error);
-//             });
-//       }
+                //     this.$router.replace('admin');
+                // })
+                .catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                if (errorCode == 'auth/weak-password') {
+                    alert('The password is too weak.');
+                } else {
+                    alert(errorMessage);
+                }
+                console.log(error);
+            });
+      }
 
-//   }
-// };
-// </script>
+  }
+};
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
---
+++ src/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+//import * as firebase from "firebase/app";
+//import firebase from 'firebase';
+//import 'firebase/firestore'
+//import 'firebase/firebase-auth'
+  import firebase from 'firebase/compat/app';
+require('firebase/compat/auth');
+ //import 'firebase/storage'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAc6k9wfoXgC4gaYvFaRcPzCBlntL0H7EU",
+  authDomain: "eli-shop.firebaseapp.com",
+  projectId: "eli-shop",
+  storageBucket: "eli-shop.appspot.com",
+  messagingSenderId: "532766581217",
+  appId: "1:532766581217:web:2ed2f79ba64ff34ba4f2a0"
+};
+
+// Initialize Firebase
+ export const fb = firebase.initializeApp(firebaseConfig);
+
+// const db = firebase.firestore();
+
+// export {fb,db}
