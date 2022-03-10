eb21fe16c19e78b9f4cdd38bd1e96cd6f59773fd
ErzaShatri <es47732@ubt-uni.net>
2022-03-10 15:11:12 +0100

    Update method
    Update method on products and realtime update

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -78,10 +78,41 @@
             </div>
       </div>
       </div>
+
+      <!-- Modal -->
+      <div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="editLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="editLabel">Edit Product</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+
+               <div class="form-group">
+                    <input type="text" placeholder="Product Name" v-model="product.name" class="form-control">
+                  </div>
+                  <div class="form-group">
+                    <input type="text" placeholder="Price" v-model="product.price" class="form-control">
+                  </div>
+
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button @click="updateProduct()" type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
   </div>
 </template>
 <script>
 import {db}from '../firebase';
+import $ from 'jquery'
 export default {
   name: "Products",
   props: {
@@ -93,10 +124,42 @@ export default {
         product: {
           name: null,
           price:null
-        }
+        },
+        activeItem: null
   }
 },
   methods:{
+    watcher(){
+          db.collection("products").onSnapshot((querySnapshot) => {
+              this.products = [];
+              querySnapshot.forEach((doc) => {
+                  this.products.push(doc);
+              });
+
+          });
+    },
+    updateProduct(){
+
+      db.collection("products").doc(this.activeItem).update(this.product)
+      .then(() => {
+          $('#edit').modal('hide');
+          this.watcher();
+          console.log("Document successfully updated!");
+      })
+      .catch((error) => {
+          // The document probably doesn't exist.
+          console.error("Error updating document: ", error);
+      });
+
+    },
+
+    editProduct(product){
+       $('#edit').modal('show');
+       this.product = product.data();
+       this.activeItem = product.id;
+    },
+
+
       deleteProduct(doc){
           if(confirm("Are you sure?")){
               db.collection("products").doc(doc).delete().then(function(){
