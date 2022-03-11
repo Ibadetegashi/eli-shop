fee23ba61ef8eaacc6c1d25bb2fbc95f6862eb54
ErzaShatri <es47732@ubt-uni.net>
2022-03-11 01:19:56 +0100

    Delete using SweetAlert
    Added some touches to the delete method with SweetAlert

--- src/main.js
+++ src/main.js
@@ -6,12 +6,26 @@ import 'jquery'
 import 'popper.js'
 import './assets/app.scss'
 import jQuery from 'jquery';
+import VueFirestore from 'vue-firestore'
+
+
+Vue.use(VueFirestore) 
 
 
 window.$ = window.jQuery = jQuery;
 
+// Swal 
+import Swal from 'sweetalert2';
 
+window.Swal = Swal;
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+    })
 
+window.Toast = Toast;
 
 Vue.config.productionTip = false
 
