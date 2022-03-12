3cf583a99595171953ba21f4ae03f0bc379fd569
Ibadetegashi <77028232+Ibadetegashi@users.noreply.github.com>
2022-03-12 04:06:04 +0100

    functions

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -28,41 +28,58 @@
                     <p>
                         Total Price : {{ this.$store.getters.totalPrice | currency }}
                     </p>
-
+ <!-- <form action="/create-checkout-session" method="POST">
+        <button type="submit"  @click="pay">Checkout</button>
+      </form> -->
+                     <button class="btn btn-primary" @click="pay">Checkout</button>
                    
-            <form>
-                    <card class='stripe-card'
-                        :class='{ complete }'
-                        stripe='pk_test_XXXXXXXXXXXXXXXXXXXXXXXX'
-                        :options='stripeOptions'
-                        @change='complete = $event.complete'
-                        />
-
-                        <button class='pay-with-stripe btn btn-primary mt-4' @click='pay' :disabled='!complete'>Pay with credit card</button>
-                    
-                    </form>
                 </div>
             </div>
 
         </div>
   </div>
 </template>
-
-
+ <script src="https://js.stripe.com/v3/"></script>
 <script>
-import NavBar from "@/components/NavBar.vue";
-
-
+import axios from 'axios';
+import NavBar from '@/components/NavBar.vue';
+var stripe = Stripe('pk_test_3yyWicMGKdLdRD28kHM3ANkX00vOa4k2B3');
 export default {
-    name: "Checkout",
-    props: {
-        msg: String
+    data() {
+        return {
+            sessionId: ""
+        };
+    },
+    methods: {
+        pay() {
+            // data = {id:10,id:10}
+            let data = this.$store.state.cart.map(item => ({ [item.productId]: item.productQuantity }));
+            data = Object.assign({}, ...data);
+            axios.get("http://localhost:5000/eli-shop/us-central1/CheckoutSession", {
+                params: {
+                    products: data
+                }
+            })
+                .then(response => {
+                this.sessionId = response.data;
+                console.log(response.data);
+                stripe.redirectToCheckout({
+                    sessionId: this.sessionId.id
+                }).then(function (result) {
+                });
+            })
+                .catch(error => {
+                console.log(error);
+            });
+        }
+    },
+    created() {
     },
     components: { NavBar }
-};
+}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
 
+
+<style>
 </style>
